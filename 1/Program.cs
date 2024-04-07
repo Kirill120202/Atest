5e93553ec27d85382a4f4a5aa8cbba357b6855df
Kerya Shirobokov <kerya_shirobokov@mail.ru>
2024-04-08 00:34:27 +0400

    1

---
+++ 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        int M = 1; // Начальное значение
+        int N = 10; // Конечное значение
+        
+        Console.WriteLine($"Натуральные числа от {M} до {N}:");
+        PrintNaturalNumbers(M, N);
+    }
+
+    static void PrintNaturalNumbers(int m, int n)
+    {
+        if (m <= n)
+        {
+            Console.WriteLine(m);
+            PrintNaturalNumbers(m + 1, n);
+        }
+    }
+}
