d63295cd88d28e347c08e76eb73dc6f6930f4526
Kerya Shirobokov <kerya_shirobokov@mail.ru>
2024-04-09 12:58:57 +0400

    3

--- 3/Program.cs
+++ 3/Program.cs
@@ -4,20 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        // Произвольный массив
         int[] array = { 1, 2, 5, 0, 10, 34 };
 
-        // Вызов рекурсивной функции для вывода элементов массива с конца
         PrintArrayReversed(array, array.Length - 1);
     }
 
-    // Рекурсивная функция для вывода элементов массива с конца
+
     static void PrintArrayReversed(int[] array, int index)
     {
         if (index >= 0)
         {
-            Console.WriteLine(array[index]); // Вывод текущего элемента
-            PrintArrayReversed(array, index - 1); // Рекурсивный вызов для предыдущего элемента
+            Console.WriteLine(array[index]);
+            PrintArrayReversed(array, index - 1);
         }
     }
 }
